940459bd8f675ecdf1eceb5d7824874e5198c282
Asus <forkmasha100@gmail.com>
2022-11-15 22:35:40 +0200

    changes 2

--- src/main/java/ReplaceDuplicates.java
+++ src/main/java/ReplaceDuplicates.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class ReplaceDuplicates {
     public static void main(String[] args) {
         int[] array = {3, 3, 4, 9, 5, 5, 6, 6, 8, 8, 10, 10, 33};
-        for (int i : array) {
+        for (int i=0;i<array.length;i++) {
             for (int j = i + 1; j < array.length; j++) {
-                if (array[i] == array[j] && i != j) {
+                if (array[i] == array[j]) {
                     array[j] = 0;
                 }
             }
--- src/main/java/SortByDesc.java
+++ src/main/java/SortByDesc.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class SortByDesc {
     public static void main(String[] args) {
-        double[] array = {2, 3, 1, 7, 11};
-        Arrays.sort(array);
+        double[] array = {2.5, 3, 1, 7.3, 11};
+        Arrays.sort(new double[][]{array}, Collections.reverseOrder());
         for (int i = array.length - 1; i >= 0; i--) {
             System.out.println(array[i] + " ");
         }
